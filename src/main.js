f51e2e8ddd7fa75678bd2982fec00a24bfcbca83
Vanchhai <chhai.chhin51@gmail.com>
2023-06-01 09:52:16 +0700

    moveing tyle to assets

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import './style.css'
+import './assets/style.css'
 import App from './App.vue'
 import router from './router'
 import i18n from './boot/i18n'
