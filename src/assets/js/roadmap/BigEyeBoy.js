9b2f4deb4a3839ac34553dfe6a3904456735d30a
benjamin <benjaminlopezyambot@gmail.com>
2023-06-03 12:00:13 +0700

    Fixed dragon for big eye boy

--- src/assets/js/roadmap/BigEyeBoy.js
+++ src/assets/js/roadmap/BigEyeBoy.js
@@ -1,13 +1,13 @@
-import RoadmapUtilities from './RoadmapUtilities'
-import BigRoad from './BigRoad'
+import RoadmapUtilities from "./RoadmapUtilities";
+import BigRoad from "./BigRoad";
 
-import _get from 'lodash/get'
-import _defaultsDeep from 'lodash/defaultsDeep'
-import _findLastIndex from 'lodash/findLastIndex'
+import _get from "lodash/get";
+import _defaultsDeep from "lodash/defaultsDeep";
+import _findLastIndex from "lodash/findLastIndex";
 
 export default class BigEyeBoy extends RoadmapUtilities {
-  constructor (_options) {
-    super()
+  constructor(_options) {
+    super();
 
     const options = _defaultsDeep(_options, {
       results: [],
@@ -15,42 +15,42 @@ export default class BigEyeBoy extends RoadmapUtilities {
       cols: 9,
       bigroadConfig: {
         rows: 6,
-        cols: 26
-      }
-    })
+        cols: 26,
+      },
+    });
 
     for (const key in options) {
-      this[key] = options[key]
+      this[key] = options[key];
     }
 
     this.bigRoadMatrix = (() => {
       const bigroad = new BigRoad({
         results: this.results,
         rows: this.bigroadConfig.rows,
-        cols: this.bigroadConfig.cols
-      })
+        cols: this.bigroadConfig.cols,
+      });
 
-      return bigroad.matrix
-    })()
+      return bigroad.matrix;
+    })();
 
-    this.traverseBigRoadScheme()
+    this.traverseBigRoadScheme();
   }
 
-  reset () {
+  reset() {
     /**
      * Define initial values
      */
-    this.previousCoordinates = [0, 0]
-    this.previousColor = null
-    this.index = 0
+    this.previousCoordinates = [0, 0];
+    this.previousColor = null;
+    this.index = 0;
 
     this.matrix = Array.from({ length: this.rows }, () => {
-      return Array.from({ length: this.cols }, () => 0)
-    })
+      return Array.from({ length: this.cols }, () => 0);
+    });
   }
 
-  traverseBigRoadScheme () {
-    this.reset()
+  traverseBigRoadScheme() {
+    this.reset();
 
     /**
      * https://www.livecasinocomparer.com/live-casino-games/live-baccarat/baccarat-roadmaps
@@ -58,24 +58,27 @@ export default class BigEyeBoy extends RoadmapUtilities {
      * If B2 is empty, the starting point is C1
      */
 
-    const B2 = [1, 1]
-    const C1 = [0, 2]
+    const B2 = [1, 1];
+    const C1 = [0, 2];
 
-    const b2HasValue = this.bigRoadMatrix[B2[0]][B2[1]]
-    const c1HasValue = this.bigRoadMatrix[C1[0]][C1[1]]
+    const b2HasValue = this.bigRoadMatrix[B2[0]][B2[1]];
+    const c1HasValue = this.bigRoadMatrix[C1[0]][C1[1]];
+    console.log(
+      "🚀 ~ file: BigEyeBoy.js:38 ~ BigEyeBoy ~ constructor ~ this.bigRoadMatrix:",
+      this.bigRoadMatrix
+    );
 
     if (!c1HasValue && !b2HasValue) {
-      return
+      return;
     }
 
-    let [initialRow, initialCol] = b2HasValue ? B2 : C1
+    let [initialRow, initialCol] = b2HasValue ? B2 : C1;
 
     while (true) {
-      const col = this.bigRoadMatrix[initialRow][initialCol]
-      const isFirstRow = initialRow === 0
+      const col = this.bigRoadMatrix[initialRow][initialCol];
+      const isFirstRow = initialRow === 0;
 
-      const nextCoordinates = this.getBigRoadCoordinatesByIndex(col.index + 1)
-
+      const nextCoordinates = this.getBigRoadCoordinatesByIndex(col.index + 1);
       /**
        * If first row, check the lenghts of previous 2 columns
        */
@@ -83,34 +86,36 @@ export default class BigEyeBoy extends RoadmapUtilities {
         /**
          * Get the column exactly to the right
          */
-        const prevColALength = this.getColumnLength(initialCol - 1)
+        const prevColALength = this.getColumnLength(initialCol - 1);
 
         /**
          * Get the 2nd column to the right
          */
-        const prevColBLength = this.getColumnLength(initialCol - 2)
+        const prevColBLength = this.getColumnLength(initialCol - 2);
 
-        this.push(prevColALength === prevColBLength ? 'red' : 'blue', {
-          big_road_index: col.index
-        })
+        this.push(prevColALength === prevColBLength ? "red" : "blue", {
+          big_road_index: col.index,
+        });
 
         if (nextCoordinates) {
-          initialRow = nextCoordinates[0]
-          initialCol = nextCoordinates[1]
-          continue
+          initialRow = nextCoordinates[0];
+          initialCol = nextCoordinates[1];
+          continue;
         } else {
-          break
+          break;
         }
       }
 
       /**
        * If non first row, check the left col and the upper left column
        */
-      const leftColLower = this.bigRoadMatrix[initialRow][initialCol - 1]
-      const leftColUpper = this.bigRoadMatrix[initialRow - 1][initialCol - 1]
+      const leftColLower = this.bigRoadMatrix[initialRow][initialCol - 1];
+      const leftColUpper = this.bigRoadMatrix[initialRow - 1][initialCol - 1];
 
-      const leftColLowerIdentity = this.identityDictionary[_get(leftColLower, 'value')]
-      const leftColUpperIdentity = this.identityDictionary[_get(leftColUpper, 'value')]
+      const leftColLowerIdentity =
+        this.identityDictionary[_get(leftColLower, "value")];
+      const leftColUpperIdentity =
+        this.identityDictionary[_get(leftColUpper, "value")];
 
       const isMatch = [
         /**
@@ -121,160 +126,182 @@ export default class BigEyeBoy extends RoadmapUtilities {
         /**
          * If they have the same identity
          */
-        leftColLowerIdentity === leftColUpperIdentity
-      ].some(Boolean)
+        leftColLowerIdentity === leftColUpperIdentity,
+        // if there's two same identity on the left
+        leftColLowerIdentity === this.identityDictionary[col.value],
+      ].some(Boolean);
 
-      this.push(isMatch ? 'red' : 'blue', {
-        big_road_index: col.index
-      })
+      this.push(isMatch ? "red" : "blue", {
+        big_road_index: col.index,
+      });
 
       if (nextCoordinates) {
-        initialRow = nextCoordinates[0]
-        initialCol = nextCoordinates[1]
-        continue
+        initialRow = nextCoordinates[0];
+        initialCol = nextCoordinates[1];
+        continue;
       } else {
-        break
+        break;
       }
     }
   }
 
-  getBigRoadCoordinatesByIndex (index) {
+  getBigRoadCoordinatesByIndex(index) {
     for (let rowIdx = 0; rowIdx < this.bigRoadMatrix.length; rowIdx++) {
-      const row = this.bigRoadMatrix[rowIdx]
+      const row = this.bigRoadMatrix[rowIdx];
 
       for (let colIdx = 0; colIdx < row.length; colIdx++) {
-        const col = row[colIdx]
+        const col = row[colIdx];
         if (col.index === index) {
-          return [rowIdx, colIdx]
+          return [rowIdx, colIdx];
         }
       }
     }
 
-    return false
+    return false;
   }
 
-  getColumnLength (columnIdx) {
-    const coordinates = [0, columnIdx]
-    const column = this.bigRoadMatrix[coordinates[0]][coordinates[1]]
-    const rootIdentity = this.identityDictionary[column.value]
+  getColumnLength(columnIdx) {
+    const coordinates = [0, columnIdx];
+    const column = this.bigRoadMatrix[coordinates[0]][coordinates[1]];
+    const rootIdentity = this.identityDictionary[column.value];
 
     /**
      * If initial column is empty, return 0
      */
     if (!column) {
-      return 0
+      return 0;
     }
 
     /**
      * Starting with one which includes the root node
      */
-    let traversalCount = 1
-    let lastIndex = column.index
-    let isEnd = false
+    let traversalCount = 1;
+    let lastIndex = column.index;
+    let isEnd = false;
 
     while (!isEnd) {
       /**
        * Check bottom if have the same identity and is the next index
        */
-      const bottomCol = _get(this.bigRoadMatrix, [coordinates[0] + 1, coordinates[1]], {})
+      const bottomCol = _get(
+        this.bigRoadMatrix,
+        [coordinates[0] + 1, coordinates[1]],
+        {}
+      );
       if (
         bottomCol.index === lastIndex + 1 &&
         rootIdentity === this.identityDictionary[bottomCol.value]
       ) {
-        lastIndex = bottomCol.index
-        traversalCount++
-        coordinates[0]++
-        continue
+        lastIndex = bottomCol.index;
+        traversalCount++;
+        coordinates[0]++;
+        continue;
       }
 
       /**
        * Check right if have the same identity and is the next index
        */
-      const rightCol = _get(this.bigRoadMatrix, [coordinates[0], coordinates[1] + 1], {})
+      const rightCol = _get(
+        this.bigRoadMatrix,
+        [coordinates[0], coordinates[1] + 1],
+        {}
+      );
       if (
         rightCol.index === lastIndex + 1 &&
         rootIdentity === this.identityDictionary[rightCol.value]
       ) {
-        lastIndex = rightCol.index
-        traversalCount++
-        coordinates[1]++
-        continue
+        lastIndex = rightCol.index;
+        traversalCount++;
+        coordinates[1]++;
+        continue;
       }
 
-      isEnd = true
+      isEnd = true;
     }
 
-    return traversalCount
+    return traversalCount;
   }
 
-  getNextCoordinates (color) {
-    const [prevRow, prevColumn] = this.previousCoordinates
+  getNextCoordinates(color) {
+    const [prevRow, prevColumn] = this.previousCoordinates;
+    console.log(
+      "🚀 ~ file: BigEyeBoy.js:212 ~ BigEyeBoy ~ getNextCoordinates ~  this.previousCoordinates:",
+      this.previousCoordinates
+    );
 
     /**
      * If initial data
      */
     if (this.previousColor === null) {
-      return [0, 0]
+      return [0, 0];
     }
 
     /**
      * If same identity
      */
     if (this.previousColor === color) {
-      const bottomPosition = _get(this.matrix, [prevRow + 1, prevColumn])
+      const bottomPosition = _get(this.matrix, [prevRow + 1, prevColumn]);
+      console.log(
+        "🚀 ~ file: BigEyeBoy.js:226 ~ BigEyeBoy ~ getNextCoordinates ~ bottomPosition:",
+        color,
+        this.matrix
+      );
 
       /**
        * If bottom position is empty
        */
       if (bottomPosition === 0) {
-        return [prevRow + 1, prevColumn]
+        return [prevRow + 1, prevColumn];
       }
 
       /**
        * Else, just increment column to the right
        */
-      return [prevRow, prevColumn + 1]
+      return [prevRow, prevColumn + 1];
     }
 
     /**
      * Fallback. If not the same identity
      */
-    const lastColIdx = _findLastIndex(this.matrix[0], Boolean)
-    const initialPosition = _get(this.matrix, [0, lastColIdx])
+    const lastColIdx = _findLastIndex(this.matrix[0], Boolean);
+    const initialPosition = _get(this.matrix, [0, lastColIdx]);
 
     if (initialPosition === 0) {
-      return [0, lastColIdx]
+      return [0, lastColIdx];
     }
 
     /**
      * Else, just increment column to the right
      */
-    return [0, lastColIdx + 1]
+    return [0, lastColIdx + 1];
   }
 
-  push (color, _options = {}) {
-    if (!['red', 'blue'].includes(color)) {
-      return console.warn(`${color} is not a valid color.`)
+  push(color, _options = {}) {
+    if (!["red", "blue"].includes(color)) {
+      return console.warn(`${color} is not a valid color.`);
     }
 
-    const options = Object.assign({
-      big_road_index: undefined
-    }, _options)
+    const options = Object.assign(
+      {
+        big_road_index: undefined,
+      },
+      _options
+    );
 
-    const [row, column] = this.getNextCoordinates(color)
+    const [row, column] = this.getNextCoordinates(color);
 
-    this.previousCoordinates = [row, column]
-    this.previousColor = color
+    this.previousCoordinates = [row, column];
+    this.previousColor = color;
 
     this.matrix[row][column] = {
       value: color,
       index: this.index++,
-      big_road_index: options.big_road_index
-    }
+      big_road_index: options.big_road_index,
+    };
 
     if (this.hasFullRow) {
-      this.matrix = this.truncateFirstColumn()
-      this.previousCoordinates = [row, column - 1]
+      this.matrix = this.truncateFirstColumn();
+      this.previousCoordinates = [row, column - 1];
     }
   }
 }
