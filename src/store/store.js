7216bfc861c8eb94963ca131d93c96863e137bd8
benjamin <benjaminlopezyambot@gmail.com>
2023-06-15 16:21:36 +0700

    Added

--- src/store/store.js
+++ src/store/store.js
@@ -4,41 +4,6 @@ export const store = reactive({
   leftDrawerOpen: false,
   isOne: true,
   menuSelect: 0,
-  // setting:   {
-  //   currency: 'usd',
-  //   usd: {
-  //     limit: 0,
-  //     limit_red: 0,
-
-  //     leisure_limit: 0,
-  //     leisure_limit_red: 0,
-
-  //     limited: 0,
-  //     limited_red: 0,
-
-  //     pair_limit: 0,
-  //     pair_limit_red: 0,
-
-  //     Lucky_limit: 0,
-  //     Lucky_limit_red: 0,
-  //   },
-  //   thb: {
-  //     limit: 0,
-  //     limit_red: 0,
-
-  //     leisure_limit: 0,
-  //     leisure_limit_red: 0,
-
-  //     limited: 0,
-  //     limited_red: 0,
-
-  //     pair_limit: 0,
-  //     pair_limit_red: 0,
-
-  //     Lucky_limit: 0,
-  //     Lucky_limit_red: 0,
-  //   }
-  // ],
   setting: {
     table_no: 0,
     bet_counter: 10,
